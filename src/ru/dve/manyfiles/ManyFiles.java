7e0e99e6d9c101c72967d1fad68aa617a5a9108c
Трушенков Дмитрий <diman28051999@gmail.com>
2017-04-28 19:15:16 +0400

    e

--- src/ru/dve/manyfiles/ManyFiles.java
+++ src/ru/dve/manyfiles/ManyFiles.java
@@ -1,7 +1,20 @@
 package ru.dve.manyfiles;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by g15oit18 on 28.04.2017.
  */
 public class ManyFiles {
+    public static void main(String[] args) throws IOException{
+        Random random = new Random();
+        File file = new File ("intdata.dat");
+        BufferedWriter numbers = new BufferedWriter(new FileWriter(file));
+        for (int i=0;i<200; i++){
+            numbers.write(String.valueOf(random.nextInt(999999)));
+            numbers.write("\r\n");
+        }
+        numbers.close();
+    }
+
 }
